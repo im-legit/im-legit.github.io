63fa0ff42de7e7fb8aafd2dcac995ff5ed953504
im-legit.com <60192115+im-legit@users.noreply.github.com>
2024-10-03 06:02:55 +0100

    Add files via upload

---
+++ assets/js/particles.config.js
@@ -0,0 +1,62 @@
+particlesJS('particles-js', {
+    "particles": {
+      "number": {
+        "value": 100,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#7A1CAC"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        }
+      },
+      "opacity": {
+        "value": 0.5,
+        "random": false
+      },
+      "size": {
+        "value": 1,
+        "random": true
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 250,
+        "color": "#7A1CAC",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 1,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": false,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": false,
+          "mode": "push"
+        }
+      }
+    },
+    "retina_detect": true
+  });
